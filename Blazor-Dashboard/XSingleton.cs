4e42935cdefe20de5f9776fe076c52ab825f7d81
djembe-waka <jamesboo8500@gmail.com>
2018-10-23 15:44:32 +0800

    Slide In Side Bar

---
+++ Blazor-Dashboard/XSingleton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDashboard
+{
+    public class XSingleton
+{
+        public static bool NavBarDisplay=true;
+
+}
+}
