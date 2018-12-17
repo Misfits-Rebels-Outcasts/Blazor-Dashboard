cb9427796ff9314ea47cffaa7e8100a0e25ee119
djembe-waka <jamesboo8500@gmail.com>
2018-12-17 13:34:59 +0800

    Remove the dependency on Javascript for the Slide In Side Bar

---
+++ Blazor-Dashboard/Pages/ClassOnlyComponent.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.AspNetCore.Blazor.RenderTree;
+using Microsoft.AspNetCore.Blazor.Layouts;
+using Blazor_Dashboard.Shared;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Blazor;
+
+namespace BlazorDashboard.Pages
+{
+    [Route("/classrender")]
+    [Layout(typeof(MainLayout))]
+    public class ClassOnlyComponent : BlazorComponent
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            Action<UIMouseEventArgs> act1 = new Action<UIMouseEventArgs>((s) =>
+            {
+                onclick(s);
+            });
+            var seq = 0;
+            builder.OpenElement(seq, "h3");
+            builder.AddContent(++seq, title);
+            builder.CloseElement();
+            builder.OpenElement(++seq, "button");
+            builder.AddAttribute(++seq, "onclick", onclick);
+            builder.AddContent(++seq, "Click");
+            builder.CloseElement();
+            base.BuildRenderTree(builder);
+        }        
+             
+        protected string title = "BuildRenderTree Action";
+        public void onclick(UIMouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello");
+            title = "Hello OnClick";
+            //StateHasChanged();
+            //ActionClicked?.Invoke(e);
+
+        }
+
+        public Action<UIMouseEventArgs> ActionClicked;
+        public async void hello()
+        {
+            System.Diagnostics.Debug.WriteLine("Hello");
+        }
+        
+    }
+}
---
+++ Blazor-Dashboard/Shapes/Rectangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle
+    {       
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+}
