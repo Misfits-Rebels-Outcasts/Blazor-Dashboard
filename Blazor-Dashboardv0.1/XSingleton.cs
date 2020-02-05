265b76882b238d152e1145c5cdf420ef30bdddfc
BlazorDashboard <jupiter@DESKTOP-PM4DFQ0.localdomain>
2020-02-05 20:59:44 +0800

    Upgrade to v1.0 Preview

---
+++ Blazor-Dashboardv0.1/XSingleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDashboard
+{
+    public class XSingleton
+{
+        public static bool NavBarDisplay=true;
+        private static XSingleton instance = null;
+
+        private XSingleton()
+        {
+        }
+
+        public static XSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new XSingleton();
+                }
+                return instance;
+            }
+        }
+        //public Blazor_Dashboard.Shared.__NavMenu NavMenu;
+
+}
+}
