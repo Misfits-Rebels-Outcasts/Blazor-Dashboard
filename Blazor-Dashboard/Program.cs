eab29eb7a74511ebf75e7b7831f9a32dba25e3e4
WebAssemblyMan <jupiter@DESKTOP-PM4DFQ0.localdomain>
2020-05-27 09:23:57 +0800

    Update to Blazor WebAssembly 3.2

--- Blazor-Dashboard/Program.cs
+++ Blazor-Dashboard/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+/*
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+*/
 
 namespace Blazor_Dashboard
 {
@@ -11,12 +23,21 @@ namespace Blazor_Dashboard
     {
         public static async Task Main(string[] args)
         {
+            /*
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddBaseAddressHttpClient();
+ 
+            await builder.Build().RunAsync();
+            */
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
+
         }
     }
 }
